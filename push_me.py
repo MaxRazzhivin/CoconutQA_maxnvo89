7ad430da4201701fea4babc95649418f9d074f60
Max_Razzhivin <max.nvo06@gmail.com>
2025-08-16 14:53:55 +0300

    add some feature for example

--- push_me.py
+++ push_me.py
@@ -1,5 +1,5 @@
 '''
-Some text with changes
+Some text with changessss
 
 git checkout main - for changing branch from secondary to main
 git merge 'name_of_secondary_branch' - for union of both branches
